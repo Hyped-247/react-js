cb79fc8f8bcc93943b2dc9678413560c56e9de89
Mohammad Mahjoub <mmahjoub@westmont.edu>
2018-11-12 14:36:11 -0800

    Practicing a bit about sending data back & forth with components.

--- src/posts/PostDetail.js
+++ src/posts/PostDetail.js
@@ -7,6 +7,7 @@ class PostDetail extends Component {
         super(props);
         // this will allow us to get the props inside the method.
         this.myTitleWasClicked = this.myTitleWasClicked.bind(this);
+        this.buttonClicked = this.buttonClicked.bind(this);
     }
 
     myTitleWasClicked(event){
@@ -17,12 +18,18 @@ class PostDetail extends Component {
         }
     }
 
+    buttonClicked(event){
+        event.preventDefault();
+        const displayButtonClicked = this.props.displayButtonClicked;
+        displayButtonClicked("The button is clicked now......")
+    }
     render() {
-      const post = this.props.post;
+    const post = this.props.post;
     return (
         <div>
             <h4 onClick={this.myTitleWasClicked}>{post.title}</h4>
             <p> {post.content}</p>
+            <button onClick={this.buttonClicked}> click me </button>
         </div>
     );
     }
--- src/posts/PostList.js
+++ src/posts/PostList.js
@@ -3,16 +3,18 @@ import PostData from '../data/posts.json';
 import PostDetail from './PostDetail';
 
 class PostList extends Component {
-
   constructor(props){
     super(props);
     // Create this for every method that you call inside your render method.
     this.handleDataCallBack = this.handleDataCallBack.bind(this)
+    this.displayButtonClicked = this.displayButtonClicked.bind(this)
   }
-
   handleDataCallBack(textMsg){
     alert(textMsg);
     console.log(this)
+  }
+  displayButtonClicked(textButtonMsg){
+    alert(textButtonMsg);
   }
   render() {
     return (
@@ -23,6 +25,7 @@ class PostList extends Component {
                   post={item}
                   key={index}
                   dataCallBack={this.handleDataCallBack}
+                  displayButtonClicked={this.displayButtonClicked}
               />;
           })}
       </div>
